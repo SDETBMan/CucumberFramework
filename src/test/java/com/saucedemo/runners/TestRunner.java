5c7a7e3ed647deac2f5377c0420f3b1335c2caff
Brian Padgett <SDETBMan-brian.padgett11@outlook.com>
2026-02-20 09:12:00 -0700

    refactor: added Healenium, Containerized with Docker, Mobile with Appium, Selenium Grid, Retry Analyzer, API Testing RestAssured, DB testing, Slack notifications, JMeter for performance, and new execution modes

--- src/test/java/com/saucedemo/runners/TestRunner.java
+++ src/test/java/com/saucedemo/runners/TestRunner.java
@@ -2,7 +2,7 @@ package com.saucedemo.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider; // <--- Import this!
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -17,8 +17,6 @@ import org.testng.annotations.DataProvider; // <--- Import this!
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    // This method is the "Turbo Button"
-    // parallel = true tells TestNG to run scenarios simultaneously
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
